4f190b83d1756eae553063aa93a4ce2d7c39c81f
Philipp | fipso <philippliermann@gmx.de>
2016-05-15 11:15:33 +0200

    update

---
+++ robocod/Bot1.java
@@ -0,0 +1,56 @@
+package de.fipso.robocod;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Bot1 - a robot by fipso100
+ */
+public class Bot1 extends Robot
+{
+	/**
+	 * run: Bot1's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,Wradar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			turnGunRight(360);
+			ahead(60);
+			turnRight(90);
+			ahead(60);
+			turnRight(90);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(20);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(120);
+	}	
+}
